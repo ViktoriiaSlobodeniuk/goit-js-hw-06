84bc0ea5cd7b39f3f297780231f6a047b5ac056b
Viktoriia Slobodeniuk <vikavas988@gmail.com>
2022-12-11 15:41:19 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,19 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsEl = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const ingredientItemEl = document.createElement("li");
+  ingredientItemEl.textContent = ingredient;
+  ingredientItemEl.classList.add("item");
+  return ingredientItemEl;
+});
+
+ingredientsEl.append(...elements);

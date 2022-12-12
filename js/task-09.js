47750d6f95a4135b58890456455e22fd6e09581a
Viktoriia Slobodeniuk <vikavas988@gmail.com>
2022-12-12 22:23:38 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const changeColorBtn = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+const colorEl = document.querySelector(".color");
+
+bodyEl.style.transitionDuration = "0.7s";
+
+changeColorBtn.addEventListener("click", bodyColorChanger);
+
+function bodyColorChanger() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }

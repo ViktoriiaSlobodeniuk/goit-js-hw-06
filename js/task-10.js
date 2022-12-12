cc6d762b64d9f18ecc5a46b8bc0b5d97dfd53632
Viktoriia Slobodeniuk <vikavas988@gmail.com>
2022-12-12 23:37:13 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,32 @@
+const inputEl = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  createBoxes(inputEl.value);
+});
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 1; i <= amount; i++) {
+    const boxEl = document.createElement("div");
+
+    boxEl.style.width = `${size}px`;
+    boxEl.style.height = `${size}px`;
+    boxEl.style.backgroundColor = getRandomHexColor();
+
+    boxesEl.append(boxEl);
+
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
